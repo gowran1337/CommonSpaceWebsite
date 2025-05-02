d3cabb8ad4197a09af407302c188f19a1219a861
Goran Nadiri <Goran.nadiri@hotmail.com>
2025-05-02 21:54:28 +0200

    added css for togglemenu

--- CommonSpaceWebsite/wwwroot/js/togglemenu.js
+++ CommonSpaceWebsite/wwwroot/js/togglemenu.js
@@ -1,4 +1,17 @@
-﻿function toggleForm() {
+﻿function togglePopup() {
 	const form = document.getElementById("formContainer");
-	form.classList.toggle("show");
+	const footer = document.getElementById("footercleaning");
+	const button = document.getElementById("popupToggleBtn");
+
+	if (form.classList.contains("show")) {
+		// Close the menu
+		form.classList.remove("show");
+		footer.classList.remove("moved");
+		button.textContent = "Add Task";
+	} else {
+
+		form.classList.add("show");
+		footer.classList.add("moved");
+		button.textContent = "Close";
+	}
 }
